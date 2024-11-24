569ab2ef2effedf03ff2aa7e7f59368f98b14f18
Dorothy Akoth <wangarraakoth@gmail.com>
2024-11-24 06:36:09 +0300

    remved register and login

--- ligiopen/settings.py
+++ ligiopen/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
      'widget_tweaks',
      'football',
-     'accounts',
+   
 ]
 
 MIDDLEWARE = [
--- ligiopen/urls.py
+++ ligiopen/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('', include('football.urls')),
-        path('', include('accounts.urls')),  # Include URLs from accounts app
+        # path('', include('accounts.urls')),  # Include URLs from accounts app
 
 
 
