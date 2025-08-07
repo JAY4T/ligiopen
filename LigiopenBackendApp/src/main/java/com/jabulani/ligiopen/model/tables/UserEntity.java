c0d8d42d5a74e9a417cf307e310162457624da6c
MbogoAlex <mbogoalex3@gmail.com>
2025-08-07 23:05:58 +0300

    make username nullable

--- LigiopenBackendApp/src/main/java/com/jabulani/ligiopen/model/tables/UserEntity.java
+++ LigiopenBackendApp/src/main/java/com/jabulani/ligiopen/model/tables/UserEntity.java
@@ -21,7 +21,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
 
     @Column(nullable = true, unique = true)
